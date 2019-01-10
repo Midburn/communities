f4bbb5adef3fa64db0d60696fc6cbcf8c47dcbe2
Nitay Rabinovich <nitayr@wix.com>
2019-01-10 18:08:08 +0200

    Some design fixes

--- src/components/controls/ExportCSV.js
+++ src/components/controls/ExportCSV.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { CSVLink } from 'react-csv';
-import { MDBIcon, MDBTooltip } from 'mdbreact';
+import { MDBIcon, MDBTooltip, MDBBtn} from 'mdbreact';
 import { withI18n } from 'react-i18next';
+import { TiDownload } from 'react-icons/ti'
 
 const CSV = (props) => {
     const {t, data, filename} = props;
@@ -10,15 +11,10 @@ const CSV = (props) => {
             <CSVLink data={data}
                      filename={filename}
                      target="_blank">
-                <MDBTooltip
-                    placement={`top`}
-                    component="button"
-                    componentClass="btn btn-primary"
-                    tag="div"
-                    color={'primary'}
-                    tooltipContent={t('export')}>
-                    <MDBIcon size="lg" icon="file-excel-o"></MDBIcon>
-                </MDBTooltip>
+                <MDBBtn outline color="primary" className="d-flex align-items-center justify-content-between">
+                    <TiDownload className="pl-1 pr-1" size={20} className="mr-1 ml-1"/>
+                    {t('export')}
+                </MDBBtn>
             </CSVLink>
         </div>
 
--- src/components/controls/TableSummery.js
+++ src/components/controls/TableSummery.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container} from 'mdbreact';
+import {Container, Table, TableBody} from 'mdbreact';
 import './TableSummery.scss';
 import {ExportCSV} from './ExportCSV';
 import {PermissableComponent} from './PermissableComponent';
@@ -10,19 +10,28 @@ import {PermissableComponent} from './PermissableComponent';
  * @constructor
  */
 export const TableSummery = ({sums, csvData, csvName}) => {
+
     return (
         <Container fluid className="TableSummeryContainer TableSummery d-flex justify-content-center z-depth-1">
             <Container fluid={window.innerWidth < 1200} className="TableSummery d-flex ">
-                <div className="SummeryContent pt-4 pb-4 d-flex justify-content-even font-small align-items-center">
-                    {Object.keys(sums || {}).map(key => {
-                        return (
-                            <span key={key}>
-                                <span className="pr-1 pl-1">{key}:</span>
-                                <span className="pr-1 pl-1">{sums[key]}</span>
-                            </span>
-                        );
-                    })}
-                    <div>
+                <div className="SummeryContent pt-4 pb-4 d-flex justify-content-between font-small align-items-center">
+                    <Table responsive borderless>
+                        <TableBody>
+                            <tr>
+                                {Object.keys(sums || {}).map(key => {
+                                    return (
+                                        <td key={key}>
+                                            <span className="pr-1 pl-1">{key}:</span>
+                                            <span className="pr-1 pl-1">{sums[key]}</span>
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+
+                        </TableBody>
+                    </Table>
+
+                    <div className="SummeryActions">
                         <PermissableComponent permitted={!!csvData}>
                             <ExportCSV
                                 data={csvData}
