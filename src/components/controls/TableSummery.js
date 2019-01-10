1cce4cdb5dad35f0e3d2e9b12be86fce54aacd10
Nitay Rabinovich <nitayr@wix.com>
2019-01-10 17:12:01 +0200

    Some linting

--- src/components/controls/TableSummery.js
+++ src/components/controls/TableSummery.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Container } from 'mdbreact';
+import {Container} from 'mdbreact';
 import './TableSummery.scss';
-import { ExportCSV } from './ExportCSV';
-import { PermissableComponent } from './PermissableComponent';
+import {ExportCSV} from './ExportCSV';
+import {PermissableComponent} from './PermissableComponent';
 
 /**
  * @param sums { [title]: sum }
@@ -12,26 +12,26 @@ import { PermissableComponent } from './PermissableComponent';
 export const TableSummery = ({sums, csvData, csvName}) => {
     return (
         <Container fluid className="TableSummeryContainer TableSummery d-flex justify-content-center z-depth-1">
-                <Container fluid={window.innerWidth < 1200} className="TableSummery d-flex ">
-                    <CardBody className="SummeryContent d-flex justify-content-even font-small align-items-center">
-                        {Object.keys(sums || {}).map(key => {
-                            return (
-                                <span key={key}>
+            <Container fluid={window.innerWidth < 1200} className="TableSummery d-flex ">
+                <CardBody className="SummeryContent d-flex justify-content-even font-small align-items-center">
+                    {Object.keys(sums || {}).map(key => {
+                        return (
+                            <span key={key}>
                                 <span className="pr-1 pl-1">{key}:</span>
                                 <span className="pr-1 pl-1">{sums[key]}</span>
                             </span>
-                            );
-                        })}
-                        <div>
-                            <PermissableComponent permitted={!!csvData}>
-                                <ExportCSV
-                                    data={csvData}
-                                    filename={csvName}
-                                ></ExportCSV>
-                            </PermissableComponent>
-                        </div>
-                    </CardBody>
-                </Container>
+                        );
+                    })}
+                    <div>
+                        <PermissableComponent permitted={!!csvData}>
+                            <ExportCSV
+                                data={csvData}
+                                filename={csvName}
+                            />
+                        </PermissableComponent>
+                    </div>
+                </CardBody>
+            </Container>
         </Container>
     );
 };
