122d5dc1deca8b39c21107f859a0950d17db5fd4
Nitay Rabinovich <nitayr@wix.com>
2019-01-16 17:21:03 +0200

    Use only `main_contact` as indication for manager

--- server/services/initial-login.js
+++ server/services/initial-login.js
@@ -49,21 +49,22 @@ class InitialLoginService {
         try {
             const groups = (await spark.get(`my_groups?eventId=${eventId}`, headers)).data.groups;
             if (groups && groups.length) {
-                for (const group of groups) {
-                    if (group.is_manager_i18n === 'yes') {
+                const groupData = (await Promise.all(groups.map(async g => (await spark.get(`camps/${g.group_id}/get`, headers)).data.camp)));
+                for (const group of groupData) {
+                    if (+group.main_contact === +sparkUser.user_id) {
                         await Promise.all([
                             permissions.addPermissionForUser({
                                 user_id: sparkUser.user_id,
                                 permission_type: constants.PERMISSION_TYPES.GIVE_PERMISSION,
                                 entity_type: constants.ENTITY_TYPE.GROUP,
-                                related_entity: group.group_id,
+                                related_entity: group.id,
                                 permitted_by: constants.GIVEN_BY_SYSTEM_CODE
                             }),
                             permissions.addPermissionForUser({
                                 user_id: sparkUser.user_id,
                                 permission_type: constants.PERMISSION_TYPES.ALLOCATE_PRESALE_TICKET,
                                 entity_type: constants.ENTITY_TYPE.GROUP,
-                                related_entity: group.group_id,
+                                related_entity: group.id,
                                 permitted_by: constants.GIVEN_BY_SYSTEM_CODE
                             })
                         ]);
