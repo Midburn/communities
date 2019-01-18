b571bb5960244bc0f97732f9df418110fc50842e
Elad Lachmi <elad.lachmi@gmail.com>
2019-01-18 19:07:17 +0200

    Corrected CD script

--- .travis/continuous_deployment.sh
+++ .travis/continuous_deployment.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ "${DEPLOY_ENVIRONMENT}" != "" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ] &&\
-   ([ "${TRAVIS_BRANCH}" == "${DEPLOY_BRANCH}" ] || ([ "${DEPLOY_TAGS}" == "true" ] && [ "${TRAVIS_TAG}" != "" ])) &&\
-   ! echo "${TRAVIS_COMMIT_MESSAGE}" | grep -- --no-deploy
+if [ "${DEPLOY_ENVIRONMENT}" != "" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "${DEPLOY_BRANCH}" ] &&\
+   [ "${TRAVIS_COMMIT_MESSAGE}" != "" ] && ! echo "${TRAVIS_COMMIT_MESSAGE}" | grep -- --no-deploy && [ "${TRAVIS_COMMIT}" != "" ]
 then
     openssl aes-256-cbc -K $encrypted_f2bd2a0d33d6_key -iv $encrypted_f2bd2a0d33d6_iv -in ./k8s-ops-secret.json.enc -out secret-k8s-ops.json -d
+    K8S_ENVIRONMENT_NAME="${DEPLOY_ENVIRONMENT}"
     OPS_REPO_SLUG="Midburn/midburn-k8s"
     OPS_REPO_BRANCH="master"
     cat secret-k8s-ops.json | docker login -u _json_key --password-stdin https://gcr.io
@@ -14,18 +14,19 @@ then
         IMAGE_TAG="gcr.io/midbarrn/midburn-communities-cd:${TRAVIS_COMMIT}"
     fi
     B64_UPDATE_VALUES=`echo '{"communities":{"image":"'${IMAGE_TAG}'"}}' | base64 -w0`
+    HELM_UPDATE_COMMIT_MESSAGE="${DEPLOY_ENVIRONMENT} volunteers image update --no-deploy"
     wget https://raw.githubusercontent.com/Midburn/midburn-k8s/master/run_docker_ops.sh
-    chmod +x run_docker_ops.sh bin/continuous_deployment.sh
-    ! ./run_docker_ops.sh "${DEPLOY_ENVIRONMENT}" "/communities/bin/continuous_deployment.sh" \
-                          "gcr.io/midbarrn/sk8s-ops" "${OPS_REPO_SLUG}" "${OPS_REPO_BRANCH}" "" "
-                            -v `pwd`:/communities
-                            -e B64_UPDATE_VALUES=${B64_UPDATE_VALUES}
-                            -e K8S_OPS_GITHUB_REPO_TOKEN=${K8S_OPS_GITHUB_REPO_TOKEN}
-                            -e OPS_REPO_SLUG=${OPS_REPO_SLUG}
-                            -e OPS_REPO_BRANCH=${OPS_REPO_BRANCH}
-                            -e B64_UPDATE_VALUES=${B64_UPDATE_VALUES}
-                            -e IMAGE_TAG=${IMAGE_TAG}
-                          " \
+    chmod +x run_docker_ops.sh
+    ! ./run_docker_ops.sh "${K8S_ENVIRONMENT_NAME}" "
+       RES=0;
+       ! ./helm_update_values.sh '${B64_UPDATE_VALUES}' '${HELM_UPDATE_COMMIT_MESSAGE}' '${K8S_OPS_GITHUB_REPO_TOKEN}' '${OPS_REPO_SLUG}' '${OPS_REPO_BRANCH}' \
+            && echo 'failed helm update values' && RES=1;
+        ! kubectl set image deployment/volunteers volunteers=${IMAGE_TAG} && echo 'failed to patch deployment' && RES=1;
+        cd /community;
+        ! gcloud container builds submit --tag $IMAGE_TAG . \
+            && echo 'failed to build communities image' && RES=1;
+        exit "'$'"RES
+    " "gcr.io/midbarrn/sk8s-ops" "${OPS_REPO_SLUG}" "${OPS_REPO_BRANCH}" "" "-v `pwd`:/communities" \
         && echo 'failed to run docker ops' && exit 1
 fi
 
