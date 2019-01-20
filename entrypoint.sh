8cdcb7d5d9e439fdbe2f5aaec4fee729b4cb16b2
Elad Lachmi <elad.lachmi@gmail.com>
2019-01-20 22:34:36 +0200

    Update entrypoint.sh
    Added user check and creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 WAIT_MSG="Waiting for mysql connection.."
 while ! echo "select 1;" | mysql "--host=${MYSQL_DB_HOST}" --protocol=tcp \
-                                 "--user=${MYSQL_DB_USERNAME}" \
-                                 "--password=${MYSQL_DB_PASSWORD}"
+                                 "--user=root" \
+                                 "--password=${MYSQL_ROOT_PASSWORD}"
 do
     [ -z "${WAIT_MSG}" ] && echo -n .
     [ ! -z "${WAIT_MSG}" ] && echo "${WAIT_MSG}" && WAIT_MSG=""
@@ -12,9 +12,15 @@ done
 
 ! echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DB_NAME};" \
   | mysql "--host=${MYSQL_DB_HOST}" --protocol=tcp \
-          "--user=${MYSQL_DB_USERNAME}" \
-          "--password=${MYSQL_DB_PASSWORD}" \
+          "--user=root" \
+          "--password=${MYSQL_ROOT_PASSWORD}" \
   && echo "Failed to create database, will try to continue anyway"
+
+! echo "GRANT ALL ON ${MYSQL_DB_NAME}.* '${MYSQL_DB_USERNAME}'@'%' IDENTIFIED BY '${MYSQL_DB_PASSWORD}'" \
+  | mysql "--host=${MYSQL_DB_HOST}" --protocol=tcp \
+          "--user=root" \
+          "--password=${MYSQL_ROOT_PASSWORD}" \
+  && echo "Failed to create user, cannot continue" && exit 1
 
 ! node_modules/.bin/sequelize db:migrate && echo "Failed migrations" && exit 1
 
