214f02dd73d2ad6957dc8954ce3356e45a9b9631
Nitay Rabinovich <nitayr@wix.com>
2019-02-16 10:53:51 +0200

    Remove group members router, Use group router

--- server/routers/v1.js
+++ server/routers/v1.js
@@ -1,52 +1,47 @@
-const Router = require('express').Router;
-const AuthRouter = require('./auth');
-const CampsRouter = require('./camps');
-const ConfigurationsRouter = require('./configurations');
-const EventsRouter = require('./events');
-const AuditRouter = require('./audit');
-const UserRouter = require('./users');
-const AllocationsRouter = require('./allocations');
-const PermissionsRouter = require('./permissions');
-const GroupMembersRouter = require('./groupMembers');
-const GroupsRouter = require('./groups');
-const RequestRouter = require('./requests');
+const Router = require ('express').Router;
+const AuthRouter = require ('./auth');
+const CampsRouter = require ('./camps');
+const ConfigurationsRouter = require ('./configurations');
+const EventsRouter = require ('./events');
+const AuditRouter = require ('./audit');
+const UserRouter = require ('./users');
+const AllocationsRouter = require ('./allocations');
+const PermissionsRouter = require ('./permissions');
+const GroupsRouter = require ('./groups');
+const RequestRouter = require ('./requests');
 
 module.exports = class V1Router {
+  constructor () {
+    this.router = new Router ();
+    this.auth = new AuthRouter ();
+    this.camps = new CampsRouter ();
+    this.configurations = new ConfigurationsRouter ();
+    this.events = new EventsRouter ();
+    this.audit = new AuditRouter ();
+    this.users = new UserRouter ();
+    this.allocations = new AllocationsRouter ();
+    this.permissions = new PermissionsRouter ();
+    this.groups = new GroupsRouter ();
+    this.requests = new RequestRouter ();
+    this.VERSION = '/v1';
+    this.initMiddleware ();
+    this.initRoutes ();
+  }
 
+  initMiddleware () {
+    // Version Router level middlewares
+  }
 
-    constructor() {
-        this.router = new Router();
-        this.auth = new AuthRouter();
-        this.camps = new CampsRouter();
-        this.configurations = new ConfigurationsRouter();
-        this.events = new EventsRouter();
-        this.audit = new AuditRouter();
-        this.users = new UserRouter();
-        this.allocations = new AllocationsRouter();
-        this.permissions = new PermissionsRouter();
-        this.groupMembers = new GroupMembersRouter();
-        this.groups = new GroupsRouter();
-        this.requests = new RequestRouter();
-        this.VERSION = '/v1';
-        this.initMiddleware();
-        this.initRoutes();
-    }
-
-    initMiddleware() {
-        // Version Router level middlewares
-    }
-
-    initRoutes() {
-        this.router.use(this.VERSION, this.auth.router);
-        this.router.use(this.VERSION, this.camps.router);
-        this.router.use(this.VERSION, this.configurations.router);
-        this.router.use(this.VERSION, this.events.router);
-        this.router.use(this.VERSION, this.audit.router);
-        this.router.use(this.VERSION, this.users.router);
-        this.router.use(this.VERSION, this.allocations.router);
-        this.router.use(this.VERSION, this.permissions.router);
-        this.router.use(this.VERSION, this.groupMembers.router);
-        this.router.use(this.VERSION, this.groups.router);
-        this.router.use(this.VERSION, this.requests.router);
-    }
+  initRoutes () {
+    this.router.use (this.VERSION, this.auth.router);
+    this.router.use (this.VERSION, this.camps.router);
+    this.router.use (this.VERSION, this.configurations.router);
+    this.router.use (this.VERSION, this.events.router);
+    this.router.use (this.VERSION, this.audit.router);
+    this.router.use (this.VERSION, this.users.router);
+    this.router.use (this.VERSION, this.allocations.router);
+    this.router.use (this.VERSION, this.permissions.router);
+    this.router.use (this.VERSION, this.groups.router);
+    this.router.use (this.VERSION, this.requests.router);
+  }
 };
