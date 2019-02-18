41d3e3cd7d3bec9b01d9d252b0de915ac6d3cf8d
Yotam Elkaslasy <yotamelkaslasy@gmail.com>
2019-02-18 11:43:51 +0200

    appreciation tickets: chart design and translation
    TODO: Integrate with API

--- src/components/controls/BarChartCard.js
+++ src/components/controls/BarChartCard.js
@@ -1,42 +1,53 @@
 import React from 'react';
-import { Card, CardBody } from 'mdbreact';
+import { Card, CardBody, MDBPopover, MDBPopoverBody } from 'mdbreact';
 import './BarChartCard.scss';
 
-export const BarChartCard = ({data, className}) => {
-    data = [
+export const BarChartCard = ({className, t, translatePrefix}) => {
+
+    const data = [
         {
-            title: 'test',
-            value: 0.3
+            title: t(`${translatePrefix}.charts.ticketsBaught`),
+            value: 0.2
         },
         {
-            title: 'test2',
-            value: 0.7
+            title: t(`${translatePrefix}.charts.reservedTickets`),
+            value: 0.54
         },
         {
-            title: 'test3',
-            value: 0.1
+            title: t(`${translatePrefix}.charts.reservedGiven`),
+            value: 0.85
         },
         {
-            title: 'test4',
-            value: 0.9
+            title:  t(`${translatePrefix}.charts.ticketOwners`),
+            value: 1
         },
 
     ];
     return (
         <Card className={`BarChartCard ${className}`}>
             <CardBody className="d-flex justify-content-between chart-container">
-                {data.map(dataItem => {
+                {data.map((dataItem) => {
                     const percent = dataItem.value * 100;
                     return (
-                        <div className="bar-container position-relative" key={dataItem.title}>
+                        <div className={`bar-container bar-container-${translatePrefix}  position-relative`} key={dataItem.title}>
                             <div className="data-area">
-                                <div className="data-container">
-                                    <div className="data-value" style={{ height: `${percent}%`, opacity: dataItem.value}}></div>
+                                <div className="data-container" >
+                                    <div className="data-value" style={{ height: `${percent}%`, minHeight: 1, background: dataItem.value !== 0 ? `rgba(105, 121, 248, ${percent/100})` : 'rgba(105, 121, 248, 1)' }}>
+                                        <MDBPopover className="btn btn-default data-popover" tag="div" placement="top">
+                                            <MDBPopoverBody>
+                                                <div>
+                                                    <div>254 שויכו</div>
+                                                    <div>46 טרם שויכו</div>
+                                                </div>
+                                            </MDBPopoverBody>
+                                        </MDBPopover>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="data-number">{`${percent}%`}</div>
                             <div className="title-area">{dataItem.title}</div>
+                            <div className="data-number">{`${percent}%`}</div>
                         </div>
+
                     )
                 })}
             </CardBody>
