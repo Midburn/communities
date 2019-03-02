4cb3d3f72961717c381ebbab58506630ab3a9258
Nitay Rabinovich <nitayrabi@gmail.com>
2019-03-02 23:48:28 +0200

    Add default where and query

--- server/controllers/requests.js
+++ server/controllers/requests.js
@@ -5,14 +5,33 @@ const constants = require('../../models/constants');
 module.exports = class RequestsController {
 
     constructor() {
+        this.DEFAULT_WHERE_OPTIONS = {
+            record_status: constants.DB_RECORD_STATUS_TYPES.ACTIVE,
+        };
         this.config = services.config;
         this.getRequests = this.getRequests.bind(this);
         this.addRequest = this.addRequest.bind(this);
     }
 
+    addQueryParamsToWhere (query, where) {
+        const updatedWhere = {...where};
+        for (const paramName in query) {
+            let param;
+            if (!this.metaParams.includes (paramName)) {
+                param = query[paramName];
+                updatedWhere[paramName] = param;
+            }
+        }
+        return updatedWhere;
+    }
+
     async getRequests(req, res, next) {
+        const where = this.addQueryParamsToWhere (
+            req.query,
+            this.DEFAULT_WHERE_OPTIONS
+        );
         try {
-            const requests = await services.db.Requests.findAll();
+            const requests = await services.db.Requests.findAll({where});
             next(new GenericResponse(constants.RESPONSE_TYPES.JSON, { requests }));
         } catch (e) {
             next(new GenericResponse(constants.RESPONSE_TYPES.ERROR, new Error(`Failed fetching requests- ${e.stack}`)));
