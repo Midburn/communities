0d1f0d528ed90d098097c567c4f69f079c8c24db
Nitay Rabinovich <nitayrabi@gmail.com>
2019-03-02 21:17:47 +0200

    Use MobX for state management

--- src/components/controls/NumberEditor.js
+++ src/components/controls/NumberEditor.js
@@ -1,34 +1,49 @@
 import React from 'react';
 import style from './NumberEditor.scss';
 import {TiPlus, TiMinus} from 'react-icons/ti'
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
 
+
+@observer
 export class NumberEditor extends React.Component {
     constructor(props) {
         super();
-        this.state = {
-            value: props.value
-        };
+        this.value = props.value;
     }
-    WAIT_INTERVAL = 1000;
+    @observable value = 0;
+    WAIT_INTERVAL = 500;
     ENTER_KEY = 13;
     timer = null;
 
     componentWillReceiveProps(props) {
-        this.setState({
-            value: props.value
-        })
+        this.value = props.value;
     }
+
+    increment = () => {
+        this.handleChange({
+            target: { value: Number(this.value) + 1 }
+        })
+    };
+
+    decrement = () => {
+        this.handleChange({
+            target: { value: Number(this.value) - 1 }
+        })
+    };
+
     handleChange = (e) => {
         const {onChange} = this.props;
         clearTimeout(this.timer);
-        this.setState({ value: e });
-        this.timer = setTimeout(() => onChange(e), this.WAIT_INTERVAL);
+        this.value = e.target.value;
+        console.log(e);
+        this.timer = setTimeout(() => onChange(this.value), this.WAIT_INTERVAL);
     };
 
     handleKeyDown = (e) => {
         const {onChange} = this.props;
         if (e.keyCode === this.ENTER_KEY) {
-            onChange(e)
+            onChange(e.target.value)
         }
     };
 
@@ -36,13 +51,13 @@ export class NumberEditor extends React.Component {
         const {onChange, min, max} = this.props;
         return (
             <div className={style.NumberCounter}>
-            <span disabled={max !== 'undefined' && this.state.value >= max} onClick={() => onChange(Number(this.state.value) + 1)} id="increment" className="handle">
+            <span disabled={max !== 'undefined' && this.value >= max} onClick={this.increment} id="increment" className="handle">
                 <TiPlus />
             </span>
-                <input type="number" data-hook="number-input" value={this.state.value || 0}
-                       onKeyDown={(e) => this.handleKeyDown(Number(e.target.value))}
-                       onChange={(e) => this.handleChange(Number(e.target.value))}/>
-                <span disabled={max !== 'undefined' && this.state.value <= min} onClick={() => onChange(Number(this.state.value) - 1)} id="decrement" className="handle">
+                <input type="number" data-hook="number-input" value={this.value}
+                       onKeyDown={this.handleKeyDown}
+                       onChange={this.handleChange}/>
+                <span disabled={max !== 'undefined' && this.value <= min} onClick={this.decrement} id="decrement" className="handle">
                 <TiMinus />
             </span>
             </div>
