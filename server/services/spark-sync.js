f8b19cd1d361476fbaeb95d31f4e9d8f917fcb85
Nitay Rabinovich <nitayr@wix.com>
2019-03-03 10:06:18 +0200

    Sync once in 4 hours. log results, use promise all (#101)

--- server/services/spark-sync.js
+++ server/services/spark-sync.js
@@ -1,81 +1,95 @@
-const db = require('./database'),
-    Sequelize = require('sequelize'),
- schedule = require('node-schedule');
+const db = require ('./database'),
+  Sequelize = require ('sequelize'),
+  schedule = require ('node-schedule');
 class SparkSync {
+  async getSparkDb () {
+    const sparkConfig = {
+      username: process.env.SPARK_DB_USER || 'spark',
+      password: process.env.SPARK_DB_PASSWORD || 'spark',
+      database: process.env.SPARK_DB_DBNAME || 'spark',
+      host: process.env.SPARK_DB_HOSTNAME ||
+        process.env.SPARK_DB_HOSTNAME ||
+        ['staging', 'production'].includes (process.env.NODE_ENV)
+        ? 'sparkdb'
+        : 'localhost',
+      dialect: 'mysql',
+      logging: false,
+    };
+    console.log (
+      `Connecting to spark db with config - ${JSON.stringify (sparkConfig)}`
+    );
+    const db = {};
+    db.sequelize = new Sequelize (sparkConfig);
+    return db;
+  }
 
-    async getSparkDb () {
-        const sparkConfig = {
-            username: process.env.SPARK_DB_USER || 'spark',
-            password: process.env.SPARK_DB_PASSWORD || 'spark',
-            database: process.env.SPARK_DB_DBNAME || 'spark',
-            host: process.env.SPARK_DB_HOSTNAME || process.env.SPARK_DB_HOSTNAME || ['staging', 'production'].includes(process.env.NODE_ENV)
-                ? 'sparkdb'
-                : 'localhost',
-            dialect: 'mysql',
-            logging: false
-        };
-        console.log (
-            `Connecting to spark db with config - ${JSON.stringify (sparkConfig)}`
-        );
-        const db = {};
-        db.sequelize = new Sequelize (sparkConfig);
-        return db;
+  async getSparkData (sparkDb, tables) {
+    const results = {};
+    for (const tableName of tables) {
+      results[
+        tableName
+      ] = await sparkDb.sequelize.query (`SELECT * FROM \`${tableName}\``, {
+        type: sparkDb.sequelize.QueryTypes.SELECT,
+      });
     }
+    return results;
+  }
 
-    async getSparkData (sparkDb, tables) {
-        const results = {};
-        for (const tableName of tables) {
-            results[
-                tableName
-                ] = await sparkDb.sequelize.query (`SELECT * FROM \`${tableName}\``, {
-                type: sparkDb.sequelize.QueryTypes.SELECT,
-            });
-        }
-        return results;
+  getUserName (user) {
+    let name;
+    if (user.first_name && user.last_name) {
+      name = `${user.first_name} ${user.last_name}`;
+      return name;
     }
-
-    getUserName (user) {
-        let name;
-        if (user.first_name && user.last_name) {
-            name = `${user.first_name} ${user.last_name}`;
-            return name;
-        }
-        try {
-            const drupalData = JSON.parse (user.addinfo_json).drupal_data.address;
-            const {first_name, last_name} = drupalData;
-            name = `${first_name} ${last_name}`;
-            return name;
-        } catch (e) {
-            name = ' ';
-            return name;
-        }
+    try {
+      const drupalData = JSON.parse (user.addinfo_json).drupal_data.address;
+      const {first_name, last_name} = drupalData;
+      name = `${first_name} ${last_name}`;
+      return name;
+    } catch (e) {
+      name = ' ';
+      return name;
     }
+  }
 
-    initSync() {
-        this.schedule = schedule.scheduleJob('*/30 * * * *', this.syncUsersCommonData.bind(this));
+  initSync () {
+    this.schedule = schedule.scheduleJob (
+      '*/4 * * *',
+      this.syncUsersCommonData.bind (this)
+    );
+  }
+
+  async syncUsersCommonData () {
+    console.log ('Starting users sync...');
+    try {
+      const sparkDb = await this.getSparkDb ();
+      const sparkData = await this.getSparkData (sparkDb, ['users']);
+      const syncResults = {
+        synced: 0,
+        failed: 0,
+      };
+      await Promise.all (
+        sparkData.users.map (async user => {
+          try {
+            await db.GroupMembers.update (
+              {
+                cell_phone: user.cell_phone,
+                name: this.getUserName (user),
+                email: user.email,
+              },
+              {where: {user_id: user.user_id}}
+            );
+            syncResults.synced++;
+          } catch (e) {
+            syncResults.failed++;
+          }
+        })
+      );
+      console.log (`Sync results - ${JSON.stringify (syncResults)}`);
+    } catch (e) {
+      console.warn (`Error syncing spark data - ${e.stack}`);
     }
-
-    async syncUsersCommonData() {
-        console.log('Starting users sync...');
-        try {
-            const sparkDb = await this.getSparkDb();
-            const sparkData = await this.getSparkData (sparkDb, ['users']);
-            for (const user of sparkData.users) {
-                try {
-
-                    await db.GroupMembers.update({
-                        cell_phone: user.cell_phone,
-                        name: this.getUserName(user),
-                        email: user.email
-                    }, {where: { user_id: user.user_id }})
-                } catch (e) {
-                }
-            }
-        } catch (e) {
-            console.warn(`Error syncing spark data - ${e.stack}`);
-        }
-    }
-
+  }
 }
 
-module.exports = new SparkSync();
+module.exports = new SparkSync ();
